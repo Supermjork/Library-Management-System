7028bf114bba798307a91808cef6dcf4cdb81f68
Supermjork <markehabaziz@gmail.com>
2022-05-11 18:03:14 +0200

    Managed to get the file to compile and window to show

--- src/LibGUI.java
+++ src/LibGUI.java
@@ -5,17 +5,17 @@ import javafx.scene.Scene;
 
 public class LibGUI extends Application {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
         StackPane layout = new StackPane();
         Scene scene = new Scene(layout, 400, 300);
 
-        primaryStage.setTitle("CodersLegacy");
+        primaryStage.setTitle("Library Project GUI Window");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
