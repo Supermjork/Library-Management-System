dd59511580fac3f52cab98bc40ba337249533400
Icen <kokeamr@hotmail.com>
2022-05-12 08:46:12 +0100

    Added a prototype for gui using AWT (preinstalled with java) instead of javafx

---
+++ src/LibGUIAwt.java
@@ -0,0 +1,58 @@
+// importing Java AWT class  
+import java.awt.*; 
+
+/**
+ * This class is used to create a GUI for the Library management system.
+ * Prototype: LibGUI.java is made to replace the original GUI that used javafx.
+ * The original GUI is harder to use and more complicated to install.
+ * @author Icen Zeyada
+ */
+public class LibGUIAwt {    
+  
+    // initializing using constructor  
+    LibGUIAwt() {  
+        
+        // creating frame
+        Frame f = new Frame("LibGUI Main Window");
+
+        // creating a button   
+        Button b = new Button("Login");  
+
+        // creating a label
+        Label l = new Label("Student ID: ");
+
+        // creating a text field
+        TextField t = new TextField();
+    
+        // setting button position on screen for all the above components
+        b.setBounds(105,140,80,30);  
+        l.setBounds(55,100,80,30);
+        t.setBounds(140,100,100,30);
+    
+        // adding the components to frame    
+        f.add(b);  
+        f.add(l);
+        f.add(t);
+    
+        // frame size 300 width and 300 height    
+        f.setSize(300,300);  
+    
+        // setting the title of Frame  
+        f.setTitle("Trying AWT");   
+            
+        // no layout manager   
+        f.setLayout(null);   
+    
+        // now frame will be visible, by default it is not visible    
+        f.setVisible(true);  
+    }    
+   
+    // main method  
+    public static void main(String args[]) {   
+    
+        // creating instance of Frame class   
+        LibGUIAwt window = new LibGUIAwt();    
+    
+    }  
+   
+}    
