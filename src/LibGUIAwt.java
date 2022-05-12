e15c072c39b0987041e76b19ac3874ba5f644f18
Icen <kokeamr@hotmail.com>
2022-05-12 23:46:18 +0100

    Added a tryin event listener. Will continue later

--- src/LibGUIAwt.java
+++ src/LibGUIAwt.java
@@ -1,7 +1,7 @@
-// importing Java AWT class
+// importing Java AWT class  
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
  * This class is used to create a GUI for the Library management system.
@@ -12,53 +12,59 @@ import java.awt.event.WindowEvent;
 public class LibGUIAwt {    
   
     // initializing using constructor  
-    LibGUIAwt() {
+    LibGUIAwt() {  
         
         // creating frame
         Frame f = new Frame("LibGUI Main Window");
-
-        // creating a button   
-        Button b = new Button("Login");
 
         // creating a label
         Label l = new Label("Student ID: ");
 
         // creating a text field
         TextField t = new TextField();
-    
+
+        
+        // creating a button   
+        Button b = new Button("Login");  
+        b.addActionListener(new ActionListener() {    
+            public void actionPerformed (ActionEvent e) {    
+                    t.setText("gay.");    
+                }    
+            }
+        );
         // setting button position on screen for all the above components
-        b.setBounds(105,140,80,30);
+        b.setBounds(105,140,80,30);  
         l.setBounds(55,100,80,30);
         t.setBounds(140,100,100,30);
     
-        // adding the components to frame
-        f.add(b);
+        // adding the components to frame    
+        f.add(b);  
         f.add(l);
         f.add(t);
     
         // frame size 300 width and 300 height    
-        f.setSize(300,300);
+        f.setSize(300,300);  
     
-        // setting the title of Frame
-        f.setTitle("Trying AWT");
+        // setting the title of Frame  
+        f.setTitle("Trying AWT");   
             
         // no layout manager   
         f.setLayout(null);   
     
         // now frame will be visible, by default it is not visible    
-        f.setVisible(true);
+        f.setVisible(true);  
 
-        // added a function to close the verdammt window
+        // closing the frame on exit
         f.addWindowListener(new WindowAdapter() {
-                                public void windowClosing(WindowEvent we) {
-                                    f.dispose();
-                                }
-                            }
-        );
+            public void windowClosing(WindowEvent we) {
+                f.dispose();
+            }
+        }
+);
     }    
    
     // main method  
-    public static void main(String[] args) {
+    public static void main(String args[]) {   
     
         // creating instance of Frame class   
         LibGUIAwt window = new LibGUIAwt();    
