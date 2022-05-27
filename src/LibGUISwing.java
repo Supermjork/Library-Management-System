27bf558d17e763a0d1f7fc7b5a205ceb4d5ae8c4
Supermjork <markehabaziz@gmail.com>
2022-05-27 23:06:47 +0200

    Have to implement the cursed GUI for the bonus points...

--- src/LibGUISwing.java
+++ src/LibGUISwing.java
@@ -1,5 +1,16 @@
 // importing Java AWT class  
+import library.LibAdmin;
+import library.LibBook;
+import library.LibStudent;
+import reader.CsvFileReader;
+import requests.LibBorrow;
+import requests.LibOrder;
+import requests.LibReserve;
+import requests.LibReturn;
+
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 
 /**
@@ -7,7 +18,7 @@ import javax.swing.*;
  * Prototype: LibGUI.java is made to replace the original GUI that used javafx.
  * The original GUI is harder to use and more complicated to install.
  */
-public class LibGUISwing {    
+public class LibGUISwing {
   
     // initializing using constructor  
     LibGUISwing() {  
@@ -19,30 +30,38 @@ public class LibGUISwing {
         JFrame studentUI = new JFrame("LibGUI Student Window");
 
         // creating a label
-        JLabel userEmailLabel = new JLabel("User Email: ");
+        JLabel userEmailLabel = new JLabel("Email: ");
+        JLabel userIDLabel = new JLabel("Phone: ");
 
         // creating a text field
         JTextField emailInField = new JTextField();
+        JTextField phoneInField = new JTextField();
 
         
         // creating a button   
         JButton loginButton = new JButton("Login");
-        loginButton.addActionListener(new ActionListener() {
-            public void actionPerformed (ActionEvent e) {
-                    emailInField.setText("nope");
-            }
-        }
-        );
+        JButton registerButton = new JButton("Registration");
+
         // setting button position on screen for all the above components
-        loginButton.setBounds(105,140,80,30);
-        userEmailLabel.setBounds(55,100,80,30);
-        emailInField.setBounds(140,100,100,30);
-    
+        loginButton.setBounds(30,200,80,30);
+        registerButton.setBounds(120, 200, 120, 30);
+
+        userEmailLabel.setBounds(40,80,80,30);
+        userIDLabel.setBounds(40,120,80,30);
+
+        emailInField.setBounds(100,80,150,30);
+        phoneInField.setBounds(100,120,150,30);
+
         // adding the components to frame    
         login.add(loginButton);
+
         login.add(userEmailLabel);
+        login.add(userIDLabel);
+
         login.add(emailInField);
-    
+        login.add(phoneInField);
+
+        login.add(registerButton);
         // frame size 300 width and 300 height    
         login.setSize(300,300);
     
@@ -60,15 +79,30 @@ public class LibGUISwing {
             public void windowClosing(WindowEvent we) {
                 login.dispose();
             }
-        }
-);
+        });
     }    
    
     // main method  
     public static void main(String[] args) {
     
         // creating instance of Frame class   
-        LibGUISwing window = new LibGUISwing();    
+        LibGUISwing window = new LibGUISwing();
+
+        List<LibBook> bookList = new ArrayList<>();
+        List<LibStudent> studentList = new ArrayList<>();
+        List<LibAdmin> adminList = new ArrayList<>();
+        List<LibOrder> orderList = new ArrayList<>();
+        List<LibBorrow> borrowList = new ArrayList<>();
+        List<LibReturn> returnList = new ArrayList<>();
+        List<LibReserve> reserveList = new ArrayList<>();
+
+        CsvFileReader.loadDataBook("src\\filebase\\books.csv", bookList);
+        CsvFileReader.loadDataStudent("src\\filebase\\students.csv", studentList);
+        CsvFileReader.loadDataAdmin("src\\filebase\\admins.csv", adminList);
+        CsvFileReader.loadDataOrder("src\\filebase\\orders.csv", orderList);
+        CsvFileReader.loadDataBorrow("src\\filebase\\borrows.csv", borrowList);
+        CsvFileReader.loadDataReturn("src\\filebase\\returns.csv", returnList);
+        CsvFileReader.loadDataReserve("src\\filebase\\reservations.csv", reserveList);
     
     }  
    
