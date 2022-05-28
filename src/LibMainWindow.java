128ac17f9588992d1abe2282b85c2de43e6a01e3
Supermjork <markehabaziz@gmail.com>
2022-05-28 19:18:22 +0200

    Worked on student panel, admin left, and shoving the UX into the UI

--- src/LibMainWindow.java
+++ src/LibMainWindow.java
@@ -1,5 +1,4 @@
 // importing Java AWT class
-import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -54,7 +53,7 @@ public class LibMainWindow extends JPanel{
         login.setSize(300,300);
     
         // setting the title of Frame  
-        login.setTitle("LibGUI Login Window");
+        login.setTitle("Login Window");
             
         // no layout manager   
         login.setLayout(null);
@@ -73,9 +72,10 @@ public class LibMainWindow extends JPanel{
     // main method
     public static void main(String[] args) {
     
-        // creating instance of Frame class   
+        // creating instance of Frame class
         LibMainWindow window = new LibMainWindow();
         LibRegisterWindow regWindow = new LibRegisterWindow();
+        LibStudentWindow studentWindow = new LibStudentWindow();
     }
 }
 
@@ -144,19 +144,13 @@ class LibRegisterWindow extends JPanel{
         });
 
 
-        regButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Should check for emptiness of text fields before appending to students.csv
-            }
+        regButton.addActionListener(e -> {
+            // Should check for emptiness of text fields before appending to students.csv
         });
 
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Needs to return to the main window "login"
-                register.dispose();
-            }
+        backButton.addActionListener(e -> {
+            // Needs to return to the main window "login"
+            register.dispose();
         });
     }
 }
@@ -168,6 +162,75 @@ class LibStudentWindow {
 
         // Creating buttons for user
 
+            // Left side
+        JButton searchBooks    = new JButton("Search for Book");
+        JTextField searchField = new JTextField();
+
+        JButton returnBorrowed = new JButton("Return a Book");
+        JTextField returnField = new JTextField();
+
+            // Right Side
+        JButton orderBook      = new JButton("Order Book");
+        JTextField orderField  = new JTextField();
+
+        JButton borrowBook     = new JButton("Borrow Book");
+        JTextField borrowField = new JTextField();
+
+            // Middle
+        JButton viewBorrowed   = new JButton("View Borrowed Books");
+        JButton exitSession    = new JButton("Log out");
+
+            // Display Panel for Search/Books in Stock/Borrowed Books
+        JScrollPane displayBooks = new JScrollPane();
+
+        // Setting boundaries
+        studentUI.setSize(750, 750);
+
+        displayBooks.setBounds(10,10,715,200);
+
+        viewBorrowed.setBounds(292,335, 175, 30);
+
+        searchBooks.setBounds(10,230,175,30);
+        searchField.setBounds(10,290,175,30);
+
+        returnBorrowed.setBounds(10,380,175,30);
+        returnField.setBounds(10,440,175,30);
+
+        orderBook.setBounds(549,230,175,30);
+        orderField.setBounds(549,290,175,30);
+
+        borrowBook.setBounds(550,380,175,30);
+        borrowField.setBounds(550,440,175,30);
+
+        exitSession.setBounds(750/3,(3*750)/4,750/3,70);
+
+        studentUI.add(viewBorrowed);
+
+        studentUI.add(searchBooks);
+        studentUI.add(searchField);
+
+        studentUI.add(returnBorrowed);
+        studentUI.add(returnField);
+
+        studentUI.add(displayBooks);
+
+        studentUI.add(orderBook);
+        studentUI.add(orderField);
+
+        studentUI.add(borrowBook);
+        studentUI.add(borrowField);
+
+        studentUI.add(exitSession);
+
+        studentUI.setLayout(null);
+        studentUI.setVisible(true);
+        studentUI.setTitle("Student View");
+
+        studentUI.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                studentUI.dispose();
+            }
+        });
     }
 }
 
